73ccbb571c8cb2b407936cda14bfefa12164df11
Unknown <regigicas@hotmail.es>
2018-12-08 13:39:54 +0100

    Login de admin

--- php/login.php
+++ php/login.php
@@ -7,6 +7,13 @@
         return;
     }
 
+    $typeLogin = isset($_POST["typeLogin"]) ? $_POST["typeLogin"] : null;
+    if ($typeLogin == null || ($typeLogin != "admin" && $typeLogin != "infractor"))
+    {
+        header("location: ../login_infractor.html");
+        return;
+    }
+
     $pass = isset($_POST['password']) ? $_POST['password'] : null;
     $credencial = isset($_POST['credencial']) ? $_POST['credencial'] : null;
     if ($pass == null || $credencial == null)
@@ -22,7 +29,12 @@
 
     include 'conexion_bd.php';
     
-    $consulta = mysqli_query($conexion, "SELECT CREDENCIAL, `PASSWORD` FROM INFRACTOR WHERE `PASSWORD` = '$pass' AND CREDENCIAL = '$credencial'") or die("No existe el usuario indicado.");
+    if ($typeLogin == "admin")
+        $sqlQuery = "SELECT credencial_admin, `password_admin` FROM admins WHERE `password_admin` = '$pass' AND credencial_admin = '$credencial'";
+    else
+        $sqlQuery = "SELECT credencial, `password` FROM infractor WHERE `password` = '$pass' AND credencial = '$credencial'";
+
+    $consulta = mysqli_query($conexion, $sqlQuery) or die("No existe el usuario indicado, error DB.");
     $nfilas = mysqli_num_rows($consulta);
     
     if ($nfilas == 0)
@@ -30,9 +42,8 @@
         print("No se ha encontrado al usuario, se te redireccionará a la página de inicio de sesión.");
         //sleep(2);
         //header("location: ../login_infractor.html");
-        header("refresh: 5; url=../login_infractor.html");
+        header("refresh: 5; url=../login_$typeLogin.html");
     }
     else
         header("location: ../login_ok.html");
-
-    ?>
+?>
